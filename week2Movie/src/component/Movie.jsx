fcfd02ce3e0d0a95f0424249eb5dede9398805f9
LGY010011 <gylm1111pm@gmail.com>
2024-10-05 20:04:55 +0900

    feat: 2주차 미션 Movie 만들기

--- week2Movie/src/component/Movie.jsx
+++ week2Movie/src/component/Movie.jsx
@@ -3,8 +3,8 @@ const Movie = ({ movie }) => { // movie prop을 구조분해 할당
     console.log(movie.poster_path); // 확인용 콘솔 로그
 
     return (
-        <div className="movie">
-            <img
+        <div>
+            <img className="movie"
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} // TMDB의 포스터 이미지를 불러옵니다.
               alt={movie.title}
             />
